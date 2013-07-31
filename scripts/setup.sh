11cbdb24cca3479943e115d4fb5a6ae8cfb65106
d-smith <doug.smith.mail@gmail.com>
2013-07-31 14:56:05 -0600

    Added the proxy config...

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 # Fuse environment setup
-# Assumes the following are done before running this script
-# export http_proxy="http://http.proxy.fmr.com:8000"
-# export https_proxy="http://http.proxy.fmr.com:8000"
-#
-# sudo apt-get install -y git-core
-# Add Defaults env_keep += "http_proxy" and env_keep += "https_proxy"
-
+export http_proxy="http://http.proxy.fmr.com:8000"
+export https_proxy="http://http.proxy.fmr.com:8000"
 sudo apt-get purge openjdk*
 sudo apt-get update
 sudo apt-get -y install python-software-properties
