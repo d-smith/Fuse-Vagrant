be182af9852d5c5cd124dd43bf9c44eee87c811d
d-smith <doug.smith.mail@gmail.com>
2013-08-19 16:03:09 +0000

    Updated setup to copy fuse distro from shared folder if present

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -23,8 +23,16 @@ sudo apt-get -y install maven
 sudo apt-get install unzip
 
 # Download and unzip our Fuse distro
+# Look for the installer in a shared folder with path /distros/fuse,
+# download it if not found.
 cd $HOME
-wget http://vc2c09dal2306.fmr.com/fuse/jboss-fuse-full-6.0.0.redhat-024.zip
+if [ -e /distros/fuse/jboss-fuse-full-6.0.0.redhat-024.zip ]
+then
+	cp /distros/fuse/jboss-fuse-full-6.0.0.redhat-024.zip $HOME
+else
+	wget http://vc2c09dal2306.fmr.com/fuse/jboss-fuse-full-6.0.0.redhat-024.zip
+fi
+
 unzip jboss-fuse-full-6.0.0.redhat-024.zip
 
 # Write out custom Fuse config on top of the distro's
